baf5a5cce09ec882ea55959ac9c10ed5fe202b73
EmaConor <159412831+EmaConor@users.noreply.github.com>
2025-12-04 15:05:33 -0500

    init - hash - cat

---
+++ ugit/cli.py
@@ -0,0 +1,40 @@
+import os
+import argparse
+import sys
+
+from . import data
+
+def main ():
+    args = parse_args()
+    args.func(args)
+
+def parse_args ():
+    parser = argparse.ArgumentParser(description='ugit - a simple git implementation')
+    
+    commands = parser.add_subparsers(title='Commands', dest='command')
+    commands.required = True
+    
+    init_parser = commands.add_parser('init', help='Initialize a new repository')
+    init_parser.set_defaults(func=init)
+    
+    hash_objet_parser = commands.add_parser('hash-object', help='Compute object ID and optionally create a blob from a file')
+    hash_objet_parser.set_defaults(func=hash_object)
+    hash_objet_parser.add_argument('file', help='The file to hash')
+    
+    cat_file_parser = commands.add_parser('cat-file', help='Provide content of repository objects')
+    cat_file_parser.set_defaults(func=cat_file)
+    cat_file_parser.add_argument('object', help='The object ID to display')
+    
+    return parser.parse_args()
+
+def init (args):
+    data.init()
+    print (f'Initializing empty ugit repository in {os.getcwd()}/{data.GIT_DIR}')
+
+def hash_object (args):
+    with open(args.file, 'rb') as f:
+        print(data.hash_object(f.read()))
+
+def cat_file (args):
+    sys.stdout.flush()
+    sys.stdout.buffer.write(data.get_object(args.object))
